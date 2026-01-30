13d120cdcf3fa0f8cae0cad84ebe7d5b369c93ae
Ankush Pal <24cs118@lingayasvidyapeeth.edu.in>
2026-01-30 19:30:33 +0530

    Some improvement in problem1.c

--- problem1.c
+++ problem1.c
@@ -2,7 +2,6 @@
 
 int main(){
     char arr[10]= {'B','a','n','a','n','a'};
-    for(int i=0; i<10; i++){
-        printf("%c ", arr[i]);
-    }    return 0;
+    printf("%s",arr);
+    return 0;
 }
