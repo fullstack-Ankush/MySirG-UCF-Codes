add3a094533aba950e94fbc56c0350474d8e5353
Ankush Pal <24cs118@lingayasvidyapeeth.edu.in>
2026-02-13 20:10:18 +0530

    Assignment 25 updated

--- Assignment 25/solution1.c
+++ Assignment 25/solution1.c
@@ -9,11 +9,52 @@ void swapVariable(int *a,int *b){
     
 
 }
+//3 . Write a fnx to convert String into UpperCase
+
+void ToUpperCase(char *str){
+    for(int i = 0;str[i];i++){
+        if(str[i]<127 || str[i]>97){
+            str[i] -= 32;
+        }
+        else
+            printf("Not possible to change into uppercase");
+    }
+
+}
 int main(){
-    int a = 5;
-    int b = 6;
+    char string[10];
+    int occurindex[10];
+
+
+
+
+
+
+
+
+
+    printf("Enter the string : ");
+    gets(string);   
+
+
+
+
+    ToUpperCase(string);
+
+
+
+
+
     
-    swapVariable(&a,&b);
-    printf("After swapping: a = %d, b = %d\n", a, b);
+    for(int i = 0;string[i];i++){
+        printf("%c",string[i]);
+    }
+    // int a = 5;
+    // int b = 6;
+    
+    // swapVariable(&a,&b);
+    // printf("After swapping: a = %d, b = %d\n", a, b);
+
+
 
 }
