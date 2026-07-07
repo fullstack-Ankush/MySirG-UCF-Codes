0ada5cb5b9f1b7bf7276daede12e20aa6d3124dd
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-07-07 21:46:49 +0530

    Updated assignment 18 & see some assignment 19

--- Assignment 18/solution6.c
+++ Assignment 18/solution6.c
@@ -1,8 +1,41 @@
 // sort element 
 
 #include<stdio.h>
+int input(int a[],int n);
+int sort(int b[]);
 
 int main(){
+
+    int a[10];
+
+    input(a,10);
+    printf("sorted array ");
+
+    sort(a);
+
+
     
 }
 
+int input(int a[],int n){
+    for(int i = 0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+}
+
+int sort(int b[]){
+    for(int i =0;i<10;i++){
+        for(int j = 0;j<10-i;j++){
+            if(b[j] > b[j+1]){
+                int temp;
+                temp = b[j+1];
+                b[j] = b[j+1];
+                b[j+1] = temp;
+            }
+        }
+        
+        printf("  %d\n",b[i]);
+    }
+
+
+}
