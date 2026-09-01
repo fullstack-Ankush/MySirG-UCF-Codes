17d71912dfc891a9c6bd4bea0e36da5b2c11ef6a
Ankush <palankush702@gmail.com>
2026-09-01 21:14:12 +0530

    Solution9 completed

---
+++ Assignment 18/solution9.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+
+
+void insert(int b[],int n);
+void reverseSort(int a[],int n);
+
+
+int main(){
+    int a[5];
+    insert(a,5);
+    reverseSort(a,5);
+    for (int i = 0;i<5;i++){
+        printf("%d ",a[i]);
+    }
+
+}
+
+void insert(int b[],int n){
+    for (int i = 0;i<n;i++){
+        if (scanf("%d",&b[i]) != 1){
+            b[i] = 0;
+        }
+    }
+}
+
+void reverseSort(int a[], int n){
+    for (int i = 0;i<n;i++){
+        for (int j = 0;j<n-i;j++){
+            if (a[j+1] > a[j]){
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+}
