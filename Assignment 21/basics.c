fc66a1832cf1d5aa348ae3f418918083e4f5c06c
Ankush <palankush702@gmail.com>
2026-09-04 09:58:37 +0530

    some modification in basics.c

--- Assignment 21/basics.c
+++ Assignment 21/basics.c
@@ -1,9 +1,30 @@
 #include<stdio.h>
 
+// int main(){
+//     int arr[10] = {'A','n','k','u','s','h'};
+//     for (int i = 0;i<10;i++)
+//         printf("%c",arr[i]);
+//     printf("hello");
+//     printf("\n");
+// }
+
+
+// int main(){
+//     int len = 0;
+//     char str[10] = {'B','H','O','P','A','L'};
+
+//     for (int i = 0;str[i];i++){
+//         len++;
+
+//     }
+//     printf("%d",len);
+// }
+
+// Taking user input as a string 
+
 int main(){
-    int arr[10] = {'A','n','k','u','s','h'};
-    for (int i = 0;i<10;i++)
-        printf("%c",arr[i]);
-    printf("hello");
-    printf("\n");
+    char str[10];
+    scanf("%s",&str);
+
+    printf("%s",str);
 }
