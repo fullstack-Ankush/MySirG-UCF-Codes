750af8eee0d24e870e8e43e8da1548a991c2e0d4
Ankush <palankush702@gmail.com>
2026-09-04 09:36:42 +0530

    today commit

---
+++ Assignment 21/basics.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int arr[10] = {'A','n','k','u','s','h'};
+    for (int i = 0;i<10;i++)
+        printf("%c",arr[i]);
+    printf("hello");
+    printf("\n");
+}
