1f2a3120149fcfbab5284a85830494c16f42327f
Ankush <palankush702@gmail.com>
2026-09-06 20:48:51 +0530

    add some more update

--- Assignment 25/practice.c
+++ Assignment 25/practice.c
@@ -1,20 +1,80 @@
 #include<stdio.h>
-void swap(int *a,int *b);
+
+int length(char *p);
 
 int main(){
-    int x = 5;
-    int y = 6;
-    // printf("%d",&x);
-    swap(&x,&y);
-    printf("%d",x);
-    printf("%d",y);
+    int l;
+    char  str[10];
+    fgets(str,10,stdin);
+    l = length(str);
+    printf("%d",l);
 }
 
-void swap(int *a,int *b){
+int length(char *p){
+    int x = 0;
+    for(int i = 0;*(p+i);i++){
+        x++;
+    }
+    return x-1;
+}
 
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void input(int x[], int n);
+// void display(int *p,int size);
+// int main(){
+//     int a[10];
+
+//     input(a,10);
+//     display(&a,10);
+
     
+//     printf("\n");
+//     return 0;
+// }
 
-}
+// void input(int x[], int n){
+//     for(int i = 0;i<n;i++){
+//         scanf("%d",&x[i]);
+//     }
+// } 
+
+// void display(int *p,int size){
+//     for (int i = 0;i<size;i++){
+//         printf("%d ",*(p+i));
+//     }
+// }
