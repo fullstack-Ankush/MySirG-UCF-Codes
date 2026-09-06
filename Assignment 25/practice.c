45eca0c6f579e924aa1d238b8195560fae8819aa
Ankush <palankush702@gmail.com>
2026-09-06 20:07:48 +0530

    some update

--- Assignment 25/practice.c
+++ Assignment 25/practice.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
+void swap(int *a,int *b);
 
 int main(){
     int x = 5;
-    int y = &x;
+    int y = 6;
     // printf("%d",&x);
-    printf("%d",*&x);
+    swap(&x,&y);
+    printf("%d",x);
     printf("%d",y);
+}
+
+void swap(int *a,int *b){
+
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    
+
 }
