d8fdadbf70bfb56444cbf0246d36c8c015da81ba
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-09-11 19:02:08 +0530

    update some pointer assignment

--- Assignment 25/retry.c
+++ Assignment 25/retry.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 void swap(int *x,int *y);
-void search_all_occurences(char *str,char ch,char *arr);
+void search_all_occurences(char *str,char ch,int *arr);
+void toUpperCase(char *str);
 int main(){
-    int x = 45;
-    int y = 30;
-    swap(&x,&y);
-    printf("%d ",x);
-    printf("%d ",y);
+    // int x = 45;
+    // int y = 30;
+    // swap(&x,&y);
+    // printf("%d ",x);
+    // printf("%d ",y);
+    char x[10] = "anamika";
+    int  result[] = {0};
+    // search_all_occurences(x,"a",result);
+    toUpperCase(x);
+    print("%d",x);
+    printf("%d",result);
 }
 
 void swap(int *a,int *b){
@@ -17,6 +24,30 @@ void swap(int *a,int *b){
 
 }
 
-void search_all_occurences(char *str,char ch,char *arr){
-    
+void search_all_occurences(char *str,char ch,int  *arr){
+    int j = 0;
+    // int occurArr[20] ={0};
+    for(int i = 0;*(str+i);i++){
+        if (*(str+i) == ch){
+            arr[j] = i;
+            j++;
+
+        }
+
+        
+    }  
+    return arr;  
+    // printf("array of indices are %d", *arr);
 }
+
+void toUpperCase(char *str){
+    for (int i = 0; str[i];i++){
+        if(str[i] >='a' && str[i] <='z'){
+
+            str[i] -= 32; 
+        }
+
+    }
+    return str;
+}
+
